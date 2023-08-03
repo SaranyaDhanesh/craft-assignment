467dec00ac0ad11c8eaa6dd830014a0dafdc4bc5
saranya-rajendran1 <saranyadhanesh11@gmail.com>
2023-08-03 12:08:38 +0530

    Create base spring boot application

---
+++ src/main/kotlin/com/demo/craftdemo/CraftDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.craftdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CraftDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<CraftDemoApplication>(*args)
+}
